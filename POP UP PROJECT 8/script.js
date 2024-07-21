df3c2f47535f1480af05fc9eb08a5dd746dcef03
RISHIKA CHAUDHARY <rishikachaudharyyy@gmail.com>
2024-07-21 13:51:54 +0530

    Add files via upload

---
+++ POP UP PROJECT 8/script.js
@@ -0,0 +1,8 @@
+let popUp = document.getElementById("pop-up");
+function openPopUp() {
+    popUp.classList.add("open-popup");
+}
+
+function closePopUp() {
+    popUp.classList.remove("open-popup");
+}
